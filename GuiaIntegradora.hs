746921eb8bc0c93c972306fe4000add1171c4e4a
LautaroCenteno <lautarocenteno.lc@gmail.com>
2025-05-04 19:48:48 -0300

    ejercicio 6 guia integradora!

--- GuiaIntegradora.hs
+++ GuiaIntegradora.hs
@@ -1,4 +1,5 @@
 module GuiaIntegradora where
+import Data.Bits (Bits(xor))
 
 --ejercicio 1
 generarStock:: [String] -> [(String, Int)]
@@ -35,6 +36,11 @@ aplicarOferta (x:xs) (y:ys) | fst x /= fst y = aplicarOferta (x:xs) (ys ++ [y])
                             | fst x == fst y && snd x <= 10 = (fst x, fromIntegral (snd x) * snd y):aplicarOferta xs (y:ys)
 
 --ejercicio 5 (Revisar si hay una forma menos rebuscada de hacer)
+type Fila = [Integer]
+type Tablero = [Fila]
+type Posicion = (Integer, Integer)
+type Camino = [Posicion]
+
 mayorFila:: Fila -> Fila
 mayorFila (x:xs) | (x:xs) == [x] = [x]
                  | x > head xs = mayorFila (x:(tail xs))
@@ -47,14 +53,39 @@ maximo (x:xs) | (x:xs) == [x] = head x
                         | head (mayorFila x) == head (mayorFila (head xs)) = maximo ((mayorFila x):(tail xs))
                         | head (mayorFila x) < head (mayorFila (head xs)) = maximo xs
 
-
-type Fila = [Integer]
-type Tablero = [Fila]
-type Posicion = (Integer, Integer)
-type Camino = [Posicion]
-
 --ejercicio 6
+conversorTablero:: Tablero -> Fila
+conversorTablero (x:xs) | (x:xs) == [x] = x
+                        | otherwise = x ++ (conversorTablero xs)
 
+quitarIntFila:: Integer -> Fila -> Fila
+quitarIntFila _ [] = []
+quitarIntFila e (x:xs) | e == x = quitarIntFila (e) (xs)
+                       | otherwise = x:(quitarIntFila e xs)
+
+quitarIntTabla:: Integer -> Tablero -> Tablero
+quitarIntTabla e (x:xs) | (x:xs) == [x] = [quitarIntFila e x]
+                        | otherwise = (quitarIntFila e x):(quitarIntTabla e xs)
+
+cantidadVecesInt:: Integer -> [Integer] -> Integer
+cantidadVecesInt _ [] = 0
+cantidadVecesInt s (x:xs) | s /= x = cantidadVecesInt (s) (xs)
+                          | s == x = 1 + cantidadVecesInt (s) (xs)
+
+listaApariciones:: Fila ->  [(Integer, Integer)]
+listaApariciones [] = []
+listaApariciones (x:xs) = [(x,cantidadVecesInt (x) (x:xs))] ++ listaApariciones (quitarIntFila (x) (xs))
+
+masRepetido:: Tablero -> Integer
+masRepetido (x:xs) | tail (listaApariciones (conversorTablero (x:xs))) == [] = fst (head (listaApariciones (conversorTablero (x:xs))))
+                   | snd (head (listaApariciones (conversorTablero (x:xs)))) > snd (head (tail (listaApariciones (conversorTablero (x:xs))))) = masRepetido (quitarIntTabla (fst (head (tail (listaApariciones (conversorTablero (x:xs)))))) (x:xs))
+                   | snd (head (listaApariciones (conversorTablero (x:xs)))) == snd (head (tail (listaApariciones (conversorTablero (x:xs))))) = masRepetido (quitarIntTabla (fst (head (tail (listaApariciones (conversorTablero (x:xs)))))) (x:xs))
+                   | snd (head (listaApariciones (conversorTablero (x:xs)))) < snd (head (tail (listaApariciones (conversorTablero (x:xs))))) = masRepetido (quitarIntTabla (fst (head (listaApariciones (conversorTablero (x:xs))))) (x:xs))
+
+--aparicionesPrimerElemento:: Fila -> Integer
+--aparicionesPrimerElemento (x:xs) | (x:xs) == [x] = 1
+--                                 | x == head xs = 1 + aparicionesPrimerElemento xs
+--                                 | x /= head xs = aparicionesPrimerElemento (x:(tail xs))
 
 --ejercicio 7
 
