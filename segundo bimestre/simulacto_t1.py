b8c107bcc174855a11c090452854383874b92412
LautaroCenteno <63695559+LautaroCenteno@users.noreply.github.com>
2025-06-17 15:46:54 -0300

    commit ejercicio simulacro

--- segundo bimestre/simulacto_t1.py
+++ segundo bimestre/simulacto_t1.py
@@ -9,8 +9,27 @@ Siendo la última modificación con la solución final:
 from queue import Queue as Cola
 
 # Ejercicio 1
+def cant_apariciones_consec(n: int, l: list[int]) -> int:
+    res: int = 0
+    racha_actual: int = 0
+    for i in range(len(l)):
+        if l[i] == n:
+            racha_actual += 1
+        else:
+            if racha_actual > res:
+                res = racha_actual
+            racha_actual = 0
+    if racha_actual > res:
+        res = racha_actual
+    return res
+
 def maximas_cantidades_consecutivos (v: list[int]) -> dict[int,int]:
-    return {}
+    res: dict[int,int] = {}
+    for i in v:
+        if i not in res:
+            res[i] = cant_apariciones_consec(i,v)
+    return res
+print(maximas_cantidades_consecutivos([2,2,2,4,3,5,2,2,6,6,6,6,7]))
  
 # Ejercicio 2
 def maxima_cantidad_primos( A: list[list[int]]) -> int:
