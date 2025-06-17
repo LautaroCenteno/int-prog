30f217b24229732ed32d08756cc7f88a84292453
LautaroCenteno <63695559+LautaroCenteno@users.noreply.github.com>
2025-06-16 21:14:57 -0300

    push notebook

--- segundo bimestre/guia8.py
+++ segundo bimestre/guia8.py
@@ -11,11 +11,11 @@ def generar_nros_al_azar(cantidad: int, desde: int, hasta: int) -> Pila[int]:
 
 #ejercicio 2
 def cantidad_elementos(p: Pila) -> int:
-    p.pop()
-
-pila: Pila = Pila()
-print(cantidad_elementos(pila))
-
+    res: int = 0
+    while not(p.empty()):
+        res += 1
+        p.get()
+    return res
 
 def imprimir_pila(p: Pila[int]) -> None:
     l: list[int] = []
@@ -71,6 +71,27 @@ def buscar_el_maximo(p: Pila[int]) -> int:
 pila: Pila[int] = generar_nros_al_azar(3,5,20)
 imprimir_pila(pila)
 print(buscar_el_maximo(generar_nros_al_azar(5,1,20)))
+"""
+
+#ejercicio 4
+def buscar_nota_maxima(p: Pila[tuple[str,int]]) -> tuple[str,int]:
+    l:list[int] = []
+    res: tuple[str,int] = p.get()
+    nota_max: int = res[1]
+    l.append(res)
+    while not(p.empty()):
+        tupla: tuple[str,int] = p.get()
+        nota: int = tupla[1]
+        l.append(tupla)
+        if nota > nota_max:
+            res = tupla
+            nota_max = nota
+    return res
+"""
+pila: Pila[tuple[str,int]] = Pila()
+pila.put(("mate", 10))
+pila.put(("ip", 9))
+print(buscar_nota_maxima(pila))
 """
 
 #ejercicio 13.1
